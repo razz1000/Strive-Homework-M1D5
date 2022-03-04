38149c7605865a04ffc2988e271ee9547a4d9adb
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-03-04 15:03:27 +0000

    Exercise 5 - DONE!

--- M1-D5.js
+++ M1-D5.js
@@ -74,7 +74,16 @@ console.log(boundary(101)); // Prints false
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let strivify = (myString) => {
+    typeof(myString) === String;
+    if (myString === "Strive") { 
+    return myString 
+} else {
+    return "Strive " + myString ;
+}   
+}
+console.log(strivify("Strive")); // Prints Strive 
+console.log(strivify("rasmus")); // Prints Strive rasmus 
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
